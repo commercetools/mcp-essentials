9bc20b4f1185baa132b6f6c7063b51a197c15132
Chukwuemeka Ajima <meeky.ae@gmail.com>
2025-07-30 00:46:53 +0200

    feat(mcp-essentials): add streamable http server
    - add functionality for streamable http mcp server
    - add options to enable both stateless and stateful streamable http server

--- typescript/src/modelcontextprotocol/index.ts
+++ typescript/src/modelcontextprotocol/index.ts
@@ -1,4 +1,6 @@
 import CommercetoolsAgentEssentials from './essentials';
+import CommercetoolsAgentEssentialsStreamable from './streamable';
 export type {Configuration} from '../types/configuration';
 export type {AvailableNamespaces} from '../types/tools';
+export {CommercetoolsAgentEssentialsStreamable};
 export {CommercetoolsAgentEssentials};
