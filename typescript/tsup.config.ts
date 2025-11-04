13583f65b9eaf6d0c8dcf43d6d49d0b81b72ac40
James Morrison <james.morrison@commercetools.com>
2025-11-04 14:51:23 +0000

    feat: added development script with watch mode enabled

--- typescript/tsup.config.ts
+++ typescript/tsup.config.ts
@@ -1,25 +1,31 @@
 import {defineConfig} from 'tsup';
 
-export default defineConfig([
-  {
-    entry: ['src/ai-sdk/index.ts'],
-    outDir: 'ai-sdk',
-    format: ['cjs', 'esm'],
-    dts: true,
-    sourcemap: true,
-  },
-  {
-    entry: ['src/modelcontextprotocol/index.ts'],
-    outDir: 'modelcontextprotocol',
-    format: ['cjs', 'esm'],
-    dts: true,
-    sourcemap: true,
-  },
-  {
-    entry: ['src/langchain/index.ts'],
-    outDir: 'langchain',
-    format: ['cjs', 'esm'],
-    dts: true,
-    sourcemap: true,
-  },
-]);
+export default defineConfig((options) => {
+  const isDev = !!(options.env?.['NODE_ENV'] === 'dev');
+  return [
+    {
+      entry: ['src/ai-sdk/index.ts'],
+      outDir: 'ai-sdk',
+      format: ['cjs', 'esm'],
+      dts: true,
+      sourcemap: true,
+      watch: isDev,
+    },
+    {
+      entry: ['src/modelcontextprotocol/index.ts'],
+      outDir: 'modelcontextprotocol',
+      format: ['cjs', 'esm'],
+      dts: true,
+      sourcemap: true,
+      watch: isDev,
+    },
+    {
+      entry: ['src/langchain/index.ts'],
+      outDir: 'langchain',
+      format: ['cjs', 'esm'],
+      dts: true,
+      sourcemap: true,
+      watch: isDev,
+    },
+  ];
+});
